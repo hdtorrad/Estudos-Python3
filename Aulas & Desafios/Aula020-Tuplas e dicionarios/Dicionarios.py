ef1fea54373a8398e5dcc69cce58a3e703d7dd88
Humberto <humbertomurad@uol.com.br>
2023-02-26 23:08:23 -0300

    Atualizações

--- Aulas & Desafios/Aula020-Tuplas e dicionarios/Dicionarios.py
+++ Aulas & Desafios/Aula020-Tuplas e dicionarios/Dicionarios.py
@@ -4,8 +4,7 @@
 # dic.keys, intuitivo né?
 # len em dict mede os pares
 # Dicts aceitam cocatenação
-# v = dic   Gera uma relação entre os dicionários e meio q se torna um só, para copiar se usa o modulo copy e deepcopy
-
+# v = dic Gera uma relação entre os dicionários e meio q se torna um só, para copiar se usa o modulo copy e deepcopy
 # Métodos úteis dos dicionários em Python
 # len - quantas chaves
 # keys - iterável com as chaves
@@ -17,6 +16,10 @@
 # pop - Apaga um item com a chave especificada (del)
 # popitem - Apaga o último item adicionado
 # update - Atualiza um dicionário com outro
+# args e kwargs
+# args (já vimos)
+# kwargs - keyword arguments (argumentos nomeados)
+
 pessoa = {
     'nome': 'Luiz Otávio',
     'sobrenome': 'Miranda',
@@ -35,3 +38,50 @@ print(list(pessoa.keys()))
 
 # for chave, valor in pessoa.items():
 #     print(chave, valor)
+
+# Empacotamento e desempacotamento de dicionários
+a, b = 1, 2
+a, b = b, a
+# print(a, b)
+
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
+pessoa = {
+    'nome': 'Aline',
+    'sobrenome': 'Souza',
+}
+
+dados_pessoa = {
+    'idade': 16,
+    'altura': 1.6,
+}
+
+pessoas_completa = {**pessoa, **dados_pessoa}
+# print(pessoas_completa)
+
+
+
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print('NÃO NOMEADOS:', args)
+
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+
+# mostro_argumentos_nomeados(nome='Joana', qlq=123)
+# mostro_argumentos_nomeados(**pessoas_completa)
+
+configuracoes = {
+    'arg1': 1,
+    'arg2': 2,
+    'arg3': 3,
+    'arg4': 4,
+}
+mostro_argumentos_nomeados(**configuracoes)
